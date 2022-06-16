4d14962209f6d0d54463a205ca723ae6bc44d3f5
cynthiadaubert <cynthiaperezdaubert@gmail.com>
2022-06-16 17:04:14 -0300

    index changes

--- router.ts
+++ router.ts
@@ -56,7 +56,7 @@ export function initRouter(container: Element) {
   }
 
   if (location.pathname == "/desafio-m5/") {
-    goTo("/desafio-m5/welcome");
+    goTo("/welcome");
   } else {
     handleRoute(location.pathname);
   /*   handleRoute("desafio-m5/welcome"); */
