3f08273144d9b18838bbc6b26281d4fbc98a9b46
cynthiadaubert <cynthiaperezdaubert@gmail.com>
2022-09-14 18:49:33 -0300

    deploy2

--- state.ts
+++ state.ts
@@ -1,123 +1,121 @@
 type Jugada = "piedra" | "papel" | "tijera";
 
 const state = {
-
   //// DATOS INICIALES ////
-data: {
+  data: {
     currentGame: {
-        computerPlay: "",
-        myPlay: "",
+      computerPlay: "",
+      myPlay: "",
     },
-    
+
     history: {
-        computer: 0,
-        me: 0,
+      computer: 0,
+      me: 0,
     },
-},
+  },
 
-//// INICIAR CON EL ESTADO GUARDADO ////
-initState(){
+  //// INICIAR CON EL ESTADO GUARDADO ////
+  initState() {
+    const localData = localStorage.getItem("saved-state");
 
-  const localData = localStorage.getItem("saved-state")
+    if (localData !== null) {
+      this.setState(JSON.parse(localData));
+    }
+  },
 
-  if (localData !== null){
-  this.setState(JSON.parse(localData))} 
-},
-  
-//// GETTER ////
-getState(){
-  return this.data;
-},
+  //// GETTER ////
+  getState() {
+    return this.data;
+  },
 
-//// SETER ////
-setState(newState) {
-  this.data = newState;
-},
+  //// SETER ////
+  setState(newState) {
+    this.data = newState;
+  },
 
-//// SETEA MOVIMIENTOS DE LAS MANOS ////
-setMove(move: Jugada) {
-  const options = ["piedra", "papel", "tijera"]
-  const currentState = this.getState();
-  currentState.currentGame.myPlay = move;
-  const randomMove = options[Math.floor(Math.random() * 3)];
-  const pcMove = currentState.currentGame.computerPlay = randomMove;
+  //// SETEA MOVIMIENTOS DE LAS MANOS ////
+  setMove(move: Jugada) {
+    const options = ["piedra", "papel", "tijera"];
+    const currentState = this.getState();
+    currentState.currentGame.myPlay = move;
+    const randomMove = options[Math.floor(Math.random() * 3)];
+    const pcMove = (currentState.currentGame.computerPlay = randomMove);
 
-  this.pushToHistory()
-}, 
+    this.pushToHistory();
+  },
 
-//// DECIDE SI GANA, PIERDE O EMPATA ////
-whoWins() {
+  //// DECIDE SI GANA, PIERDE O EMPATA ////
+  whoWins() {
+    const currentState = this.getState();
+    const myPlay = currentState.currentGame.myPlay;
+    const computerPlay = currentState.currentGame.computerPlay;
 
-  const currentState = this.getState();
-  const myPlay = currentState.currentGame.myPlay;
-  const computerPlay = currentState.currentGame.computerPlay; 
+    const ganeConTijeras = myPlay == "tijera" && computerPlay == "papel";
+    const ganeConPiedra = myPlay == "piedra" && computerPlay == "tijera";
+    const ganeConPapel = myPlay == "papel" && computerPlay == "piedra";
 
-  const ganeConTijeras = myPlay == "tijera" && computerPlay == "papel";
-  const ganeConPiedra = myPlay == "piedra" && computerPlay == "tijera";
-  const ganeConPapel = myPlay == "papel" && computerPlay == "piedra";
+    const pcGanaTijeras = myPlay == "papel" && computerPlay == "tijera";
+    const pcGanaPiedra = myPlay == "tijera" && computerPlay == "piedra";
+    const pcGanaPapel = myPlay == "piedra" && computerPlay == "papel";
 
-  const pcGanaTijeras = myPlay == "papel" && computerPlay == "tijera";
-  const pcGanaPiedra = myPlay == "tijera" && computerPlay == "piedra";
-  const pcGanaPapel = myPlay == "piedra" && computerPlay == "papel";
-  
-  const win = [ganeConTijeras || ganeConPiedra || ganeConPapel].includes(true);
-  const lose = [pcGanaPapel || pcGanaPiedra || pcGanaTijeras].includes(true);
+    const win = [ganeConTijeras || ganeConPiedra || ganeConPapel].includes(
+      true
+    );
+    const lose = [pcGanaPapel || pcGanaPiedra || pcGanaTijeras].includes(true);
 
-  let result
-  
-  if (win == true) {
+    let result;
+
+    if (win == true) {
       result = "win";
-  } else if (lose == true){
-      result = "lose"
-  }else {
+    } else if (lose == true) {
+      result = "lose";
+    } else {
       result = "tie";
-  } 
+    }
 
-  return result
-},
+    return result;
+  },
 
-//// GUARDA LOS PUNTOS SEGUN EL RESULTADO DE WHO WINS ////
-pushToHistory() {
-const result = this.whoWins()
-const currentState = this.getState();
-const computerScore = currentState.history.computer;
-const myScore = currentState.history.me;
+  //// GUARDA LOS PUNTOS SEGUN EL RESULTADO DE WHO WINS ////
+  pushToHistory() {
+    const result = this.whoWins();
+    const currentState = this.getState();
+    const computerScore = currentState.history.computer;
+    const myScore = currentState.history.me;
 
-  if (result == "win") {
-    this.setState({
-      ...currentState,
-      history: {
-        computer: computerScore,
-        me: myScore + 1, 
-      },
-    });
-  }
-  if (result == "lose") {
-    this.setState({
-      ...currentState,
-      history: {
-        computer: computerScore + 1,
-        me: myScore,
-      },
-    
-    });
-  }
-  if (result == "tie") {
-    this.setState({
-      ...currentState,
-      history: {
-        computer: computerScore,
-        me: myScore,
-      },
-    
-    });
-  }
+    if (result == "win") {
+      this.setState({
+        ...currentState,
+        history: {
+          computer: computerScore,
+          me: myScore + 1,
+        },
+      });
+    }
+    if (result == "lose") {
+      this.setState({
+        ...currentState,
+        history: {
+          computer: computerScore + 1,
+          me: myScore,
+        },
+      });
+    }
+    if (result == "tie") {
+      this.setState({
+        ...currentState,
+        history: {
+          computer: computerScore,
+          me: myScore,
+        },
+      });
+    }
 
-  //// SETEA ESTE NUEVO ESTADO EN EL LOCALSTORAGE ////
-  localStorage.setItem("saved-state", JSON.stringify(state.getState()))
+    //// SETEA ESTE NUEVO ESTADO EN EL LOCALSTORAGE ////
+    localStorage.setItem("saved-state", JSON.stringify(state.data));
+  },
+};
 
-},
+export { state };
 
-}
-
-export {state}
+/* localStorage.setItem("saved-state", JSON.stringify(state.getState())) */
